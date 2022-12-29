83d6072275cbb60c6d01b659e7427bf27bd57086
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-12-29 18:05:35 +0530

    created the constructors,getter amd setter , tostring into the Track.java

--- src/main/java/com/niit/Muzix/domain/Track.java
+++ src/main/java/com/niit/Muzix/domain/Track.java
@@ -15,4 +15,56 @@ public class Track {
     private String trackName;
     private int trackRating;
     private Artist artist;
+
+    public Track() {
+    }
+
+    public Track(int trackId, String trackName, int trackRating, Artist artist) {
+        this.trackId = trackId;
+        this.trackName = trackName;
+        this.trackRating = trackRating;
+        this.artist = artist;
+    }
+
+    public int getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(int trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public int getTrackRating() {
+        return trackRating;
+    }
+
+    public void setTrackRating(int trackRating) {
+        this.trackRating = trackRating;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    @Override
+    public String toString() {
+        return "Track{" +
+                "trackId=" + trackId +
+                ", trackName='" + trackName + '\'' +
+                ", trackRating=" + trackRating +
+                ", artist=" + artist +
+                '}';
+    }
 }
