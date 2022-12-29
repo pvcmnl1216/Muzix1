8d84e93f3ed70e273b5b2b9a5fae6bb2b2b5580a
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-12-29 18:55:48 +0530

    created the findByArtist Method inside the MuzixRepository.java

---
+++ src/main/java/com/niit/Muzix/repository/MuzixRepository.java
@@ -0,0 +1,13 @@
+package com.niit.Muzix.repository;
+
+import com.niit.Muzix.domain.Track;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+public interface MuzixRepository extends MongoRepository<Track,Integer> {
+
+    @Query("{'artist.artistName':{$in:[?0]}}")
+    public List<Track> findByArtist(String artistName);
+}
